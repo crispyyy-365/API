60d2eccfe6d9e912ff850f6843f6e46ad1f94717
Huseyn Abbasov <huseynaa-bp217@code.edu.az>
2024-12-23 23:59:59 +0400

    initial commit

---
+++ MyApi1/Controllers/CategoryController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using MyApi1.DAL;
+using MyApi1.Entity;
+
+namespace MyApi1.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class CategoryController : ControllerBase
+	{
+		public AppDbContext _context { get; set; }
+		public CategoryController(AppDbContext context)
+		{
+			_context = context;
+		}
+		[HttpGet]
+		public async Task<IActionResult> Get()
+		{
+			var categories = await _context.Categories.ToListAsync();
+			return StatusCode(StatusCodes.Status200OK, categories);
+		}
+		[HttpGet("{id}")]
+		public async Task<IActionResult> Get(int id)
+		{
+			if (id < 1) return BadRequest();
+			Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+			if (category == null) return NotFound();
+			return StatusCode(StatusCodes.Status200OK, category);
+		}
+		[HttpPost]
+		public async Task<IActionResult> Create(Category category)
+		{
+			await _context.Categories.AddAsync(category);
+			await _context.SaveChangesAsync();	
+			return StatusCode(StatusCodes.Status201Created, category);
+		}
+		[HttpPut("{id}")]
+		public async Task<IActionResult> Update(int id, string name)
+		{
+			if (id < 1) return BadRequest();
+			Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+			if (category == null) return NotFound();
+			category.Name = name;
+			await _context.SaveChangesAsync();
+			return StatusCode(StatusCodes.Status204NoContent);
+		}
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> Delete(int id)
+		{
+			if (id < 1) return BadRequest();
+			Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+			if (category == null) return NotFound();
+			_context.Categories.Remove(category);
+			await _context.SaveChangesAsync();
+			return StatusCode(StatusCodes.Status204NoContent);
+		}
+	}
+}
---
+++ MyApi1/DAL/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApi1.Entity;
+
+namespace MyApi1.DAL
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+		public DbSet<Category> Categories { get; set; }
+	}
+}
